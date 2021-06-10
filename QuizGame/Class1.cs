0efe4f896c3490af70bedab926b96d2e4cb8b2ef
TrudyFox <Trudy.Mk@outlook.com>
2021-06-10 12:31:18 -0700

    Add project files.

---
+++ QuizGame/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    class GameCard
+    {
+        public string GameQuestion;
+        public List<string> AnswerOptions = new List<string>();
+        public int GameAnswer;
+
+
+    }
+}
---
+++ QuizGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<GameCard> GameCards = new List<GameCard>();
+
+
+            GameCard A = new GameCard();
+
+            GameCard B = new GameCard();
+
+            A.GameQuestion = "whats Oprahs last name?";
+            A.AnswerOptions.Add("Barney");
+            A.AnswerOptions.Add("Winfrey");
+            A.AnswerOptions.Add("Fox");
+            A.GameAnswer = (1);
+
+
+
+            B.GameQuestion = "how many hats does Oprah own?";
+            B.AnswerOptions.Add("eight");
+            B.AnswerOptions.Add("one");
+            B.AnswerOptions.Add("twenty");
+            B.GameAnswer = (1);
+
+            for (int i = 0; i > 2; i++) 
+            { 
+               
+
+
+            }
+        }
+    }
+}
