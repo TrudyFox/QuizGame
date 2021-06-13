a37a73067ba363119ad69102a20eadfb63142cd1
TrudyFox <Trudy.Mk@outlook.com>
2021-06-13 05:20:48 -0700

    added for each loop console.writeline question

--- QuizGame/Program.cs
+++ QuizGame/Program.cs
@@ -8,19 +8,14 @@ namespace QuizGame
         static void Main(string[] args)
         {
             List<GameCard> GameCards = new List<GameCard>();
-
-
             GameCard A = new GameCard();
-
             GameCard B = new GameCard();
 
             A.GameQuestion = "whats Oprahs last name?";
             A.AnswerOptions.Add("Barney");
-            A.AnswerOptions.Add("Winfrey");
+            A.AnswerOptions.Add("Wnfrey");
             A.AnswerOptions.Add("Fox");
             A.GameAnswer = (1);
-
-
 
             B.GameQuestion = "how many hats does Oprah own?";
             B.AnswerOptions.Add("eight");
@@ -28,9 +23,23 @@ namespace QuizGame
             B.AnswerOptions.Add("twenty");
             B.GameAnswer = (1);
 
-            for (int i = 0; i > 2; i++) 
-            { 
-               
+            GameCards.Add(A);
+            GameCards.Add(B);
+
+     
+
+            foreach (GameCard gc in GameCards) // i think i need to declar the var in gamecard
+            {                             // i tried class, and List, and string though i didnt think it would work
+                
+                //print the question
+                Console.WriteLine(gc.GameQuestion);
+
+
+                // print all the answers
+
+                //get user guess / input
+
+                //check if its the right guess
 
 
             }
